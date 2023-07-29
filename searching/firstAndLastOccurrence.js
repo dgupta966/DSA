e1bb5bbdeb8d34e6a24686b0580183276d082f11
Deepak Gupta <dgupta966@gmail.com>
2023-07-29 10:59:49 +0530

    last occurrence of element in an array

--- searching/firstAndLastOccurrence.js
+++ searching/firstAndLastOccurrence.js
@@ -25,6 +25,31 @@ function firstOccurrence(arr, k) {
 
 console.log(firstOccurrence(arr, 5));
 
+function lastOccurrence(arr, k) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] < k) {
+      low = mid + 1;
+    } else if (arr[mid] > k) {
+      high = mid - 1;
+    } else {
+      if (mid === arr.length - 1) {
+        return mid;
+      } else if (arr[mid + 1] !== k) {
+        return mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+  }
+  return -1;
+}
+
+console.log(lastOccurrence(arr, 5));
+
 /*
 time complexity - O(N)
 space - O(1)
