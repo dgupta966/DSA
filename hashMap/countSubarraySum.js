16511112e10bb6339eae8a086646a3baac3dddda
Deepak Gupta <dgupta966@gmail.com>
2023-08-08 11:47:27 +0530

    count subarray sum

--- hashMap/countSubarraySum.js
+++ hashMap/countSubarraySum.js
@@ -1,6 +1,6 @@
 const arr = [2, 1, 4, -3, -2, 3, -1, -2, 6];
 
-function longestSubarraySum(arr, k) {
+function countSubarraySum(arr, k) {
   const m = new Map();
   let ans = 0;
   let pSum = 0;
@@ -10,13 +10,12 @@ function longestSubarraySum(arr, k) {
     if (!m.has(pSum - k)) {
       m.set(pSum, i);
     } else {
-      ans = Math.max(ans, i - m.get(pSum - k));
+      ans++;
     }
   }
   //   console.log(m);
   //   console.log(ans);
-
   return ans;
 }
 
-console.log(longestSubarraySum(arr, 15));
+console.log(countSubarraySum(arr, 0));
