b2babfea06d7b66b645dfded30575655f146c678
Deepak Gupta <dgupta966@gmail.com>
2023-08-08 07:17:26 +0530

    check sum

---
+++ hashMap/checkSum.js
@@ -0,0 +1,37 @@
+// find sum of any pair in array
+
+const arr = [4, 3, 2, 7, 1, 7];
+
+function checkPair(arr, k) {
+  const m = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (m.get(arr[i])) {
+      let value = m.get(arr[i]);
+      m.set(arr[i], ++value);
+    } else {
+      m.set(arr[i], 1);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let searchedElement = k - arr[i];
+    if (m.get(searchedElement)) {
+      let freq = m.get(searchedElement);
+      if (arr[i] === searchedElement && freq > 1) {
+        return "True";
+      } else if (arr[i] != searchedElement) {
+        return "True";
+      }
+    }
+  }
+
+  return "False";
+}
+
+console.log(checkPair(arr, 7));
+
+/* Time complexity - logn
+   Space complexity - logn
+
+*/
