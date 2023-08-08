c5e3aaa9249902edb83b6617cabd0cfe9a176e08
Deepak Gupta <dgupta966@gmail.com>
2023-08-08 10:59:42 +0530

    longest subarray sum

---
+++ hashMap/LongestSubarraySum.js
@@ -0,0 +1,22 @@
+const arr = [2, 1, 4, -3, -2, 3, -1, -2, 6];
+
+function LongestSubarraySum(arr, k) {
+  const m = new Map();
+  let ans = 0;
+  let pSum = 0;
+  m.set(pSum, -1);
+  for (let i = 0; i < arr.length; i++) {
+    pSum += arr[i];
+    if (!m.has(pSum - k)) {
+      m.set(pSum, i);
+    } else {
+      ans = Math.max(ans, i - m.get(pSum - k));
+    }
+  }
+  //   console.log(m);
+  //   console.log(ans);
+
+  return ans;
+}
+
+console.log(LongestSubarraySum(arr, 15));
