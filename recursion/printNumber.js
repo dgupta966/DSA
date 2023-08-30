2ae1acc9e3f85ffc9762e1ae967ec6e96146b5c7
Deepak Gupta <dgupta966@gmail.com>
2023-08-30 16:56:27 +0530

    print number n to 1 - recursion

--- recursion/printNumber.js
+++ recursion/printNumber.js
@@ -5,3 +5,24 @@ function printNumber(n) {
 }
 
 printNumber(10);
+
+/* 
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
+
+function printNumberNtoOne(n) {
+  if (n == 0) return;
+  console.log(n);
+  printNumberNtoOne(n - 1);
+}
+
+printNumberNtoOne(10);
